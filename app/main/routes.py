115d7c5c1e6ae6e08c2124efcfada2f6c03165d4
dasis <dasiscodes@gmail.com>
2026-09-19 15:20:47 +0300

    favicon added

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, request, Blueprint
+import os
+from flask import render_template, request, Blueprint, current_app, send_from_directory
 from app.models import Post
 
 from flask import Blueprint
@@ -17,3 +18,12 @@ def home():
 @main.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+
+@main.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(current_app.root_path, "static", "pics"),
+        "favicon.png",
+        mimetype="image/png",
+    )
